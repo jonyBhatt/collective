c3f28f8453cd1671a48b8a47b89d3545d39a74b6
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-04-21 21:58:34 +0600

    Ui components and styles of the global app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,7 @@ import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
     <div className="p-4 space-y-4">
-      <Button className="" variant={"elevated"}>
-        Button
-      </Button>
-      <Input placeholder="this is input" />
-      <Progress value={50} />
-      <Textarea placeholder="Write something here" />
+      <p>Hello</p>
     </div>
   );
 }
